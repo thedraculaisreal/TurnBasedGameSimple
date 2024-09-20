7db2c0c0213dc0920b520f567d5475b1e9f1065d
thedraculaisreal <79720230+thedraculaisreal@users.noreply.github.com>
2024-09-20 20:27:36 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,29 @@
 int main() 
 {
 
+	while (true) 
+	{
+		game::runGame();
 
-	game::runGame();
+		char input;
+		std::cout << "Would you like to play again? (Y/N): ";
+		std::cin >> input;
+
+		if (input == 'N' || input == 'n')
+		{
+			break;
+		}
+		else if (input == 'Y' || input == 'y')
+		{
+			continue;
+		}
+		else
+		{
+			std::cout << "Invalid input" << std::endl;
+		}
+		
+	}
+
 
 	return 0;
 }
